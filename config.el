31e60d93b6688061487430d89f4fc9560cc1cf39
Nesterov-BA <nesterov.boris123@gmail.com>
2026-09-14 16:23:49 +0300

    Snippets, latex pretty symbols and tex-fmt

--- config.el
+++ config.el
@@ -1,51 +1,14 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+(add-to-list 'exec-path (expand-file-name "~/.local/bin"))
+(add-to-list 'exec-path (expand-file-name "~/.local/share/cargo/bin"))
+(add-to-list 'exec-path (expand-file-name "~/.config/doom/helpers/.venv/bin"))
+(add-to-list 'exec-path "/usr/local/bin")
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 20)
       doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 20)
       doom-big-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 24)
       doom-symbol-font (font-spec :family "JetBrainsMono Nerd Font Mono"))
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
-;; Specify both a dark and light theme, like so and Doom will choose which one
-;; to load based on your system light/dark setting:
-;;
-;;   (setq doom-theme '(doom-one   . doom-one-light))   ; (DARK . LIGHT)
-;;
-;; If you want more pro-active theme switching based on OS light/dark mode, look
-;; up the `auto-dark' package.
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -53,40 +16,8 @@
 (setq org-directory "~/org/")
 
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `with-eval-after-load' block, otherwise Doom's defaults may override your
-;; settings. E.g.
-;;
-;;   (with-eval-after-load 'PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look them up).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;; ---------------------------------------------------------------------------
 ;; LaTeX / AucTeX configuration (VimTeX equivalent)
 ;; ---------------------------------------------------------------------------
-(add-to-list 'exec-path (expand-file-name "~/.config/doom/helpers/.venv/bin"))
 (setq apheleia-log-debug-info t)
 (after! latex
   ;; --- viewer: Zathura ------------------------------------------------
@@ -103,7 +34,24 @@
   (setq TeX-show-compilation nil
         TeX-after-compilation-finished-functions
         (remove 'TeX-revert-document-buffer
-                TeX-after-compilation-finished-functions)))
+                TeX-after-compilation-finished-functions))
+  (add-hook 'LaTeX-mode-hook #'prettify-symbols-mode)
+
+  ;; Optional: Improve the editing experience
+  (setq prettify-symbols-unprettify-at-point 'right-edge)
+  (with-eval-after-load 'tex-mode
+    (dolist (pair '(("\\varnothing" . ?∅)
+                    ("\\Rightarrow" . ?⇒)
+                    ("\\rightarrow" . ?→)
+                    ("\\mapsto" . ?↦)
+                    ("\\leq" . ?≤)
+                    ("\\geq" . ?≥)
+                    ("\\neq" . ?≠)
+                    ("\\approx" . ?≈)
+                    ("\\cdot" . ?·)
+                    ("\\times" . ?×)
+                    ("\\ldots" . ?…)))
+      (add-to-list 'tex--prettify-symbols-alist pair))))
 
 (after! ispell
   (setq ispell-program-name "hunspell")
@@ -113,3 +61,18 @@
   ;; Advise ispell-hunspell-add-multi-dice to correctly parse the comma-separated list
   (ispell-set-spellchecker-params)
   (ispell-hunspell-add-multi-dic "en_US,ru_RU"))
+
+;; Define tex-fmt as an Apheleia formatter and use it for LaTeX modes
+(with-eval-after-load 'apheleia
+  (when (executable-find "tex-fmt")
+    ;; Register the formatter
+    (setf (alist-get 'tex-fmt apheleia-formatters)
+          '("tex-fmt" "--stdin"))
+
+    ;; Point LaTeX modes at it
+    (dolist (mode '(latex-mode LaTeX-mode TeX-latex-mode TeX-mode))
+      (setf (alist-get mode apheleia-mode-alist) 'tex-fmt))))
+
+(set-file-template! "/Makefile$"
+  :trigger "__makefile-cpp"
+  :mode 'makefile-gmake-mode)
